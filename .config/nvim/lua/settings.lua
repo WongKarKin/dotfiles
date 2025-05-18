3b37c4e8f87806e6e9f07400fba9d44cc56801a5
WongKarKin <kinkwwork@gmail.com>
2025-05-18 23:46:34 +0800

    added nvim config

---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,54 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = true
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+
+vim.o.mouse = 'a'
+
+vim.o.showmode = false
+
+vim.schedule(function ()
+	vim.o.clipboard = 'unnamedplus'
+end)
+
+vim.o.breakindent = true
+
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+
+vim.o.smartcase = true
+
+vim.o.signcolumn = 'yes'
+
+vim.o.updatetime = 250
+
+vim.o.splitright = true
+
+vim.o.splitbelow = true
+
+vim.o.list = true
+
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.o.inccommand = 'split'
+
+vim.o.cursorline = true
+
+vim.o.scrolloff = 8
+
+vim.o.confirm = true
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
